d85d8ad51775c5f5718c69374ee9bffa60cff630
Mohammed Abdullah <mohammed.aladhary@outlook.com>
2023-07-25 13:18:08 +0400

    API Requests and Responses

---
+++ CountryInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace httpRequest
+{
+    internal class CountryInfo
+    {
+        String officialName;
+        String capital;
+        Double area;
+
+        public CountryInfo(String officialName, String capital, Double area)
+        {
+            this.officialName = officialName;
+            this.capital = capital;
+            this.area = area;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+
+namespace httpRequest
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            List<CountryInfo> response = await callAPI();
+            //Console.WriteLine(response);
+        }
+
+        public async static Task<List<CountryInfo>> callAPI()
+        {
+            string api = "https://restcountries.com/v3.1/all?fields=name,capital,area";
+            HttpClient client = new HttpClient();
+            HttpResponseMessage responseMessage = await client.GetAsync(api);
+            List<CountryInfo> countries = new List<CountryInfo>();
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                string stringResponse = await responseMessage.Content.ReadAsStringAsync();
+                JsonDocument document = JsonDocument.Parse(stringResponse);
+                JsonElement element = document.RootElement;
+                if(element.ValueKind == JsonValueKind.Array)
+                {
+                    var enumerator = element.EnumerateArray;
+                    foreach ( JsonElement country in element.EnumerateArray()) 
+                    {
+                        string capital = "";
+                        if (country.GetProperty("capital").GetArrayLength() > 0)
+                        {
+                            capital = country.GetProperty("capital")[0].ToString();
+                        }
+                        Double area = country.GetProperty("area").GetDouble();
+                        String officialName = country.GetProperty("name").GetProperty("official").GetString();
+                        countries.Add(new CountryInfo(officialName, capital, area));
+                    }
+                }
+            }
+            return countries;
+        }
+    }
+}
